8d2c36b88caf1fdc5633db103ec25eb47a5af664
Vincent Ip <vincent@qomorebi.net>
2022-02-20 17:26:47 +0100

    graphql endpoint

---
+++ pages/api/graphql.ts
@@ -0,0 +1,58 @@
+import { ApolloServer, gql } from "apollo-server-micro";
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled,
+} from "apollo-server-core";
+
+import Cors from "micro-cors";
+
+const cors = Cors();
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+    user(username: String): User
+  }
+  type User {
+    name: String
+    username: String
+  }
+`;
+const users = [
+  { name: "Leeroy Jenkins", username: "leeroy" },
+  { name: "Foo Bar", username: "foobar" },
+];
+
+const resolvers = {
+  Query: {
+    users: () => users,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
+});
+const startServer = server.start();
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default cors(async function handler(req, res) {
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+  await startServer;
+
+  await server.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+});
