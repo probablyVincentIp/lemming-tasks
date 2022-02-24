a4ddec30b85b8aa19b63b8cc6318cff1f1fa8544
Vincent Ip <ip@sport-thieme.de>
2022-02-24 20:38:10 +0100

    graphql queries

--- grapqhl/Query.ts
+++ grapqhl/Query.ts
@@ -1,10 +1,58 @@
-import { queryType } from "nexus";
+import { intArg, nonNull, queryType } from "nexus";
 
 export default queryType({
   definition(t) {
     t.field("nexusfoobar", {
       type: "String",
       resolve: () => "hi",
+    });
+
+    t.field("tag", {
+      type: "Tag",
+      args: { id: nonNull(intArg()) },
+      resolve: async function (_root, { id }, ctx, _info) {
+        return await ctx.db.tag.findFirst({
+          where: {
+            id,
+          },
+        });
+      },
+    });
+
+    t.field("project", {
+      type: "Project",
+      args: { id: nonNull(intArg()) },
+      resolve: async function (_root, { id }, ctx, _info) {
+        return await ctx.db.project.findFirst({
+          where: {
+            id,
+          },
+        });
+      },
+    });
+
+    t.field("task", {
+      type: "Task",
+      args: { id: nonNull(intArg()) },
+      resolve: async function (_root, { id }, ctx, _info) {
+        return await ctx.db.task.findFirst({
+          where: {
+            id,
+          },
+        });
+      },
+    });
+
+    t.field("user", {
+      type: "User",
+      args: { id: nonNull(intArg()) },
+      resolve: async function (_root, { id }, ctx, _info) {
+        return await ctx.db.user.findFirst({
+          where: {
+            id,
+          },
+        });
+      },
     });
   },
 });
