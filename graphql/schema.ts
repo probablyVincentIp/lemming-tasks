95e9a03c2e0705198d839ff7f9e215bf5836a948
Vincent Ip <ip@sport-thieme.de>
2022-02-26 07:09:14 +0100

    typegen to types folder, and some cleanup

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,35 +1,56 @@
 import { makeSchema } from "nexus";
-import path from "path";
+import { join } from "path";
+
 import Project from "./models/Project";
 import Tag from "./models/Tag";
 import Task from "./models/Task";
 import User from "./models/User";
 import Mutation from "./Mutation";
+import createProject from "./mutations/createProject";
+import createTag from "./mutations/createTag";
+import createTask from "./mutations/createTask";
 import createUser from "./mutations/createUser";
-
+import updateProject from "./mutations/updateProject";
+import updateTag from "./mutations/updateTag";
+import updateTask from "./mutations/updateTask";
+import updateUser from "./mutations/updateUser";
 import Query from "./Query";
+
+/**
+ * use path.join() as we are in a nextjs environement
+ * https://stackoverflow.com/a/68670795/4111389
+ **/
+
 export default makeSchema({
   shouldGenerateArtifacts: true,
   outputs: {
-    typegen: join(process.cwd(), "generated/nexus-typegen.ts"),
-    schema: join(process.cwd(), "generated/schema.graphql"),
+    typegen: join(
+      process.cwd(),
+      "node_modules/@types/nexus-typegen/index.d.ts"
+    ),
+    schema: join(process.cwd(), ".generated/schema.graphql"),
   },
 
   contextType: {
     export: "Context",
     alias: "ContextModule",
-    module: join(
-      process.cwd(),
-      "graphql/context.ts"
-    ) /* https://stackoverflow.com/a/68670795/4111389 */,
+    module: join(process.cwd(), "graphql/context.ts"),
   },
-  // sourceTypes: {
-  //   modules: [
-  //     {
-  //       module: require.resolve(".prisma/client/index.d.ts"),
-  //       alias: "db",
-  //     },
-  //   ],
-  // },
-  types: [Query, Mutation, createUser, User, Task, Project, Tag],
+
+  types: [
+    Query,
+    Mutation,
+    createProject,
+    createTag,
+    createTask,
+    createUser,
+    Project,
+    Tag,
+    Task,
+    updateProject,
+    updateTag,
+    updateTask,
+    updateUser,
+    User,
+  ],
 });
